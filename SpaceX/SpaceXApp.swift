aad2d9c859b45225252a5ec05b9bd60869d3eb91
Kfir Breger <kfir@conopa.co>
2021-01-23 20:46:46 +0100

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Kfir Breger on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
